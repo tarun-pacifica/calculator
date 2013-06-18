40f0594e7d0d46d546741ee5c78c1d385c485264
Tarun M <zoilism@gmail.com>
2013-06-18 15:57:19 +1000

    updated main file to require simple or advanced calc

--- main.rb
+++ main.rb
@@ -1,6 +1,22 @@
 # Authors
 
 # Dan
-# Tarun Mookhey
+# Tarun
 # Mike 
 # Will
+
+require_relative 'advanced_calc'
+require_relative 'simple_calc'
+
+response = menu
+
+while response != 'q'
+if ('b' == response)
+	simple_calc
+	elseif ('a' == response)
+advanced_calc
+end
+
+response = menu
+end
+
