811cb67298684c1a3a686df47e662755b8c503b9
Tarun M <zoilism@gmail.com>
2013-06-18 17:01:05 +1000

    updated text to close loop

--- main.rb
+++ main.rb
@@ -9,7 +9,7 @@
 #require_relative 'simple_calc'	
 
 
-puts "Welcome to our Calculator. Choose Simple (s) or Advanced (a) functions here: "
+puts "Gday! Dan, Tarun, Mike and Will welcome you to the SUPER-CALCULATOR. Choose Simple (s) or Advanced (a) functions here: "
 response = gets.chomp
 
 #response = menu
@@ -17,8 +17,10 @@ response = gets.chomp
 while response != 'q'
 if ('s' == response)
 #simple_calc
-	elsif ('a' == response)
+elsif ('a' == response)	
 #advanced_calc
+else
+	puts "You can only choose from s or a. Choose Simple (s) or Advanced (a) functions here, Try again now: "
 end
 
 response = gets.chomp
