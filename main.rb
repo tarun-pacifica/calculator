405a2f840d3a0901fbad2d03f1efe709e6d76001
Dan Addington <daniel.anthony.addington@gmail.com>
2013-06-18 17:03:31 +1000

    updated advanced

--- main.rb
+++ main.rb
@@ -1,5 +1,27 @@
 # Authors
 
 # Dan
-# Tarun Mookhey
+# Tarun
+# Mike 
+# Will
 
+#require_relative 'advanced_calc' 
+#require_relative 'simple_calc'	
+
+
+puts "Gday! Dan, Tarun, Mike and Will welcome you to the SUPER-CALCULATOR. Choose Simple (s) or Advanced (a) functions here: "
+response = gets.chomp
+
+#response = menu
+
+while response != 'q'
+if ('s' == response)
+#simple_calc
+elsif ('a' == response)	
+#advanced_calc
+else
+	puts "You can only choose from s or a. Choose Simple (s) or Advanced (a) functions here, Try again now: "
+end
+
+response = gets.chomp
+end
